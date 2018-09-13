6b1992c8e4eaba776fb6c682096b2ec76b566a3b
Yuanfeng Xin <yuanfengxin@yuanfengdemacbook-pro.local>
2018-09-13 17:24:35 -0600

    Initial Commit

---
+++ address_variable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  address_variable
+//
+//  Created by Yuanfeng Xin on 2018/9/13.
+//  Copyright © 2018年 Yuanfeng Xin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
